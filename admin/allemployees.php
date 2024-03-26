9ae3d8736f04438300001e319164ddc7e962afd8
Rajesh Dutta <68239141+rajeshdutta2000@users.noreply.github.com>
2024-03-27 02:30:36 +0530

    Add files via upload

---
+++ admin/allemployees.php
@@ -0,0 +1,166 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+//error_reporting(0);
+if (strlen($_SESSION['aid']==0)) {
+  header('location:logout.php');
+  } else{
+
+
+if(isset($_GET['delid']))
+  {
+    $eid=$_GET['delid'];
+    $query=mysqli_query($con,"delete employeedetail,empexpireince,empeducation from employeedetail
+left join empexpireince on empexpireince.EmpID=employeedetail.ID
+left join empeducation on empeducation.EmpID=employeedetail.ID
+where employeedetail.ID='$eid'");
+    echo "<script>alert('Record Deleted successfully');</script>";
+    echo "<script>window.location.href='allemployees.php'</script>";
+  }
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Staff Details</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+  <?php include_once('includes/sidebar.php')?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+         <?php include_once('includes/header.php')?>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">Staff Details</h1>
+
+<p style="font-size:16px; color:red" align="center"> <?php if($msg){
+    echo $msg;
+  }  ?> </p>
+
+ <div class="table-responsive">
+<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+
+<tr>
+  <th>S no.</th>
+  <th>staff Code</th>
+  <th>staff First Name</th>
+  <th>staff Last Name</th>
+  <th>staff Email</th>
+  <th>staff Contact no</th>
+  <th>staff Joining Date</th>
+  <th>Action</th>
+  
+</tr>
+
+<?php
+$ret=mysqli_query($con,"select * from employeedetail");
+$cnt=1;
+while ($row=mysqli_fetch_array($ret)) {
+
+?>
+
+<tr>
+  <td><?php echo $cnt;?></td>
+  <td><?php  echo $row['EmpCode'];?></td>
+   <td><?php echo $row['EmpFname'];?></td>
+    <td><?php echo $row['EmpLName'];?></td>
+  <td><?php echo $row['EmpEmail'];?></td>
+  <td><?php echo $row['EmpContactNo'];?></td>
+  <td><?php echo $row['EmpJoingdate'];?></td>
+  <td><a href="editempprofile.php?editid=<?php echo $row['ID'];?>">Edit Profile Details</a> | 
+   <a href="editempeducation.php?editid=<?php echo $row['ID'];?>">Edit Education Details</a> |
+    <a href="editempexp.php?editid=<?php echo $row['ID'];?>">Edit Experience Details</a>  |
+    <a href="allemployees.php?delid=<?php echo $row['ID'];?>" onclick="return confirm('Do you really want to delete');" style="color:red">Delete</a>
+  </td>
+</tr>
+
+<?php 
+$cnt=$cnt+1;
+}?>
+
+</table>
+
+</div>
+
+
+
+
+
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+   <?php include_once('includes/footer.php');?>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="../js/sb-admin-2.min.js"></script>
+  
+  <!-- Page level plugins -->
+  <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="../js/demo/datatables-demo.js"></script>
+</body>
+
+</html>
+<?php }  ?>
